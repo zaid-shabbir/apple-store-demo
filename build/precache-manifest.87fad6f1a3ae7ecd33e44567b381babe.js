ee1008accf8efd490d7e69e76124a6ff4083f5cc
zaid-shabbir <zaidshabbir17@gmail.com>
2020-05-12 02:15:38 +0500

    Build Fixed

---
+++ build/precache-manifest.87fad6f1a3ae7ecd33e44567b381babe.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa1640d4345a57e23c921aa7bebecf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48b3c5dc6f2eb9e1d6a9",
+    "url": "/static/css/main.3f100f49.chunk.css"
+  },
+  {
+    "revision": "0c17fff9fb2dc8cfcee5",
+    "url": "/static/js/2.6ae8e781.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6ae8e781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b3c5dc6f2eb9e1d6a9",
+    "url": "/static/js/main.fff450b6.chunk.js"
+  },
+  {
+    "revision": "97e6239babaf5a27b079",
+    "url": "/static/js/runtime-main.d33835c5.js"
+  },
+  {
+    "revision": "cf1ef3063942ea14c038081dd85b95e9",
+    "url": "/static/media/apple-gray.cf1ef306.png"
+  },
+  {
+    "revision": "ab92446929392ef11fd06208a169a477",
+    "url": "/static/media/apple-iphonexs-max-gold-back-2.ab924469.png"
+  },
+  {
+    "revision": "22e2a38eddf6da914ba4e7b343c493ea",
+    "url": "/static/media/apple-iphonexs-max-gold.22e2a38e.png"
+  },
+  {
+    "revision": "aaaa2f8dc072aea186b610c89f971e24",
+    "url": "/static/media/iphone1.aaaa2f8d.png"
+  },
+  {
+    "revision": "423388cdd3296b16244213ac0e48e796",
+    "url": "/static/media/iphone4.423388cd.png"
+  },
+  {
+    "revision": "3475cd9f0ad563a3ceb428c09b4fa49b",
+    "url": "/static/media/laptop.3475cd9f.png"
+  },
+  {
+    "revision": "9d8b1929bc4ffea7bc6d9646127bcb84",
+    "url": "/static/media/mac.9d8b1929.png"
+  },
+  {
+    "revision": "2e11614cafb07a5ba2320d94f193c4b4",
+    "url": "/static/media/shutterstock_-2.2e11614c.png"
+  },
+  {
+    "revision": "f07f119f871a08ca8527640eb5b462f0",
+    "url": "/static/media/watch.f07f119f.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/db/precache-manifest.ec850d540ed8f475207cc0733934f31c.js"
+  "/precache-manifest.87fad6f1a3ae7ecd33e44567b381babe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/db/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
